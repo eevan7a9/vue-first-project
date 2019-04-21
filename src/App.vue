5ee32e609b43b67da469f99f3a385d7dda096e53
eevan7a9 <e7a9@protonmail.com>
2019-04-21 21:37:25 +0900

    my First Commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h4>{{msg}}</h4>
+    <Todos v-bind:todos="todos"></Todos>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+export default {
+  name: "app",
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "first",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "second",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "third",
+          completed: false
+        }
+      ],
+      msg: "Hello World"
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul v-bind:key="todo.id" v-for="todo in todos">
+      <li>{{todo.title}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"]
+};
+</script>
+<style scoped>
+</style>
