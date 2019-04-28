de4d625c1f240764973b40f455035c6603fc81c4
eevan7a9 <e7a9@protonmail.com>
2019-04-28 18:28:30 +0900

    added axios get,post,delete

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Todos from "./components/Todos";
 import HeaderLayout from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
 
 export default {
   name: "app",
@@ -20,35 +21,48 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Learn Vuejs",
-          completed: true
-        },
-        {
-          id: 2,
-          title: "Learn Django",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Learn Wordpress Theme Development",
-          completed: false
-        }
-      ],
-      msg: "Hello World"
+      todos:[],
+      // todos: [
+      //   {
+      //     id: 1,
+      //     title: "Learn Vuejs",
+      //     completed: true
+      //   },
+      //   {
+      //     id: 2,
+      //     title: "Learn Django",
+      //     completed: false
+      //   },
+      //   {
+      //     id: 3,
+      //     title: "Learn Wordpress Theme Development",
+      //     completed: false
+      //   }
+      // ],
     };
   },
   methods: {
     delItem(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch();
       // console.log(id)
     },
     addItem(newItem) {
-      this.todos = [...this.todos, newItem];
+      const {title, completed} = newItem;
+      axios.post('https://jsonplaceholder.typicode.com/todos?_limit=5', {
+        title:title,
+        completed:completed
+      })
+      .then((res) => this.todos = [...this.todos, res.data])
+      .catch();
+    },
+  },
+  created(){ /** created() will run right away */
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((res) => this.todos = res.data)
+      .catch();
     }
-  }
 };
 </script>
 
